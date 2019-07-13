41fe1e7296c70c39c52e2ebc6b60ba0c1690425b
Sergio Varela <sergiovarelaok@gmail.com>
2019-07-12 21:57:44 -0300

    primera etapa

---
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -0,0 +1,34 @@
+import React, { Component } from 'react';
+import Button from '../../UI/Button/Button';
+
+class OrderSummary extends Component {
+  componentWillUpdate() {
+    console.log('[OrderSummary] WillUpdate');
+  }
+
+  render () {
+    const ingredientSummary = Object.keys(this.props.ingredients)
+      .map(igKey => {
+        return (
+          <li key={igKey}>
+            <span style={{ textTransform: 'capitalize' }}>{igKey}</span>: {this.props.ingredients[igKey]}
+          </li>
+        );
+      });
+    return (
+      <>
+        <h3>Your Order</h3>
+        <p>A delicious burger with the following ingredients:</p>
+        <ul>{ingredientSummary}</ul>
+        <p><strong>Total Price: {this.props.price.toFixed(2)}</strong></p>
+        <p>Continue to Checkout?</p>
+        <div style={{ textAlign: 'right' }}>
+          <Button btnType="Danger" clicked={this.props.purchaseCancelled}>CANCEL</Button>
+          <Button btnType="Success" clicked={this.props.purchaseContinued}>CONTINUE</Button>
+        </div>
+      </>
+    );
+  }
+};
+
+export default OrderSummary;
---
+++ src/containers/Layout/Layout.js
@@ -0,0 +1,34 @@
+import React, { Component } from 'react';
+import classes from './Layout.module.css';
+import Toolbar from '../../components/Navigation/Toolbar/Toolbar';
+import SideDrawer from '../../components/Navigation/SideDrawer/SideDrawer';
+
+class Layout extends Component {
+    state = {
+        showSideDrawer: false
+    }
+
+    sideDrawerCloseHandler = () => {
+        this.setState({ showSideDrawer: false });
+    }
+
+    sideDrawerToggleHandler = () => {
+        this.setState((prevState) => {
+            return { showSideDrawer: !prevState.showSideDrawer }
+        });
+    }
+
+    render () {
+        return (
+            <>
+                <Toolbar drawerToggleClicked={this.sideDrawerToggleHandler} />
+                <SideDrawer open={this.state.showSideDrawer} closed={this.sideDrawerCloseHandler} />
+                <main className={classes.Content}>
+                    {this.props.children}
+                </main>
+            </>
+        );
+    }
+};
+
+export default Layout;

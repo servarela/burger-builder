4f5a8c1daec12d8fdaf22318aa02947cc17b3fd7
Sergio Varela <sergiovarelaok@gmail.com>
2019-07-28 20:50:20 -0300

    form config & validation

--- src/components/UI/Button/Button.js
+++ src/components/UI/Button/Button.js
@@ -3,9 +3,9 @@ import classes from './Button.module.css';
 
 const Button = (props) => (
   <button 
+    disabled={props.disabled}
     className={[classes.Button, classes[props.btnType]].join(' ')}
-    onClick={props.clicked} 
-  >{props.children}</button>
+    onClick={props.clicked}>{props.children}</button>
 );
 
 export default Button;
